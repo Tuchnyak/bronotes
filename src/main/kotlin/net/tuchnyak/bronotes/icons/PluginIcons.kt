8226f28638c28ddbc0c4f2add0db9dc49392bd66
tuchnyak <georgii.o.sv@gmail.com>
2024-11-28 23:49:15 +0400

    WCDA-210: toolWindowIcon.svg

---
+++ src/main/kotlin/net/tuchnyak/bronotes/icons/PluginIcons.kt
@@ -0,0 +1,10 @@
+package net.tuchnyak.bronotes.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object PluginIcons {
+
+    @JvmField
+    val toolWindow = IconLoader.getIcon("/icons/toolWindowIcon.svg", javaClass)
+
+}
